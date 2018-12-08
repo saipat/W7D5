a8858e1c9f3ad823fec50fca9b7273c5c2499ffb
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-12-07 17:29:25 -0800

    Add sessionReducer and entitiesReducer

---
+++ benchSai/frontend/reducers/entities_reducer.js
@@ -0,0 +1,3 @@
+import { combineReducers } from 'redux';
+import usersReducer from './user_reducer';
+
---
+++ benchSai/frontend/reducers/users_reducer.js
@@ -0,0 +1,15 @@
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+import merge from 'loadash/merge';
+
+
+const userReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch(action.type){
+    case RECEIVE_CURRENT_USER: 
+      return merge({}, state, {[action.user.id]: action.user});
+    default:
+      return state;
+  }
+};
+
+export default usersReducer;
